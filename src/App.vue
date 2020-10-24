96eadd900c27d0309136d8e1f1be368a499903d0
David <haldw@live.co.uk>
2020-10-24 19:49:16 +0400

    Initial commit - functional but ugly

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
+// src/App.vue
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>To Do List</h1>
+    <input
+      v-model="currentTodo"
+      @keydown.enter="addTodo()"
+      placeholder="Add a task"
+    />
+    <button @click="addTodo()">Add</button>
+    <hr />
+
+    <!-- Task entries -->
+    <ul class="todos">
+      <li v-for="todo in todos" :key="todo.id">
+        <!-- checkbox -->
+        <input type="checkbox" id="checkbox" v-model="todo.completed" />
+
+        <span
+          @dblclick="editTodo(todo)"
+          v-show="editTodoId !== todo.id"
+          :class="{ completed: todo.completed }"
+        >
+          {{ todo.label }}
+        </span>
+
+        <!-- editTodo input field -->
+        <input
+          type="editTask"
+          v-model="todo.label"
+          v-show="editTodoId == todo.id"
+        />
+        <button @click="saveEdit()">Save</button>
+
+        <!-- remove task -->
+        <button @click="removeTodo(todo)">Remove</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+      editTodoId: null,
+    };
+  },
+
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false, // reactive to todo.completed checkbox
+      });
+      this.currentTodo = ""; // add task then clear enty field
+    },
+
+    editTodo(todo) {
+      this.editTodoId = todo.id;
+    },
+
+    saveEdit() {
+      this.editTodoId = null;
+    },
+
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.editing {
+  display: none;
 }
 </style>
