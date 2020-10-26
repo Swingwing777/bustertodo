6e4185d2c63cb0547791c3a5e4cafbfa6a1d1a95
David <haldw@live.co.uk>
2020-10-26 18:46:23 +0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-  publicPath: '/bustertodo/'
-}
+// module.exports = {
+//   publicPath: '/bustertodo/'
+// }
 
 
 
-// File contents must be commented out for localhosting
+// File contents must be commented out for localhosting and uncommitted for deplying to GitHub pages.
